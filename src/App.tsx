c098967d2204eaebc29b03f74c5ac6dc26b8d955
StephenSHorton <4stephenhorton@gmail.com>
2023-02-12 13:49:45 -0700

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+//3 is the lowest 13 is the highest
+//no aces or 2's
+//jokers are
+
+import { useState } from 'react'
+
+const values = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+function valueFilter(value: number) {
+  switch (value) {
+    case 11:
+      return 'J'
+    case 12:
+      return 'Q'
+    case 13:
+      return 'K'
+    default:
+      return value
+  }
+}
+
+export default function App() {
+  const [history, setHistory] = useState<number[]>([])
+  const [total, setTotal] = useState(0)
+  return (
+    <main className="w-screen h-screen">
+      <div className="flex flex-col justify-around h-full items-center relative">
+        <div className="absolute top-10 left-1/2 -translate-x-1/2 max-h-[100px] overflow-y-scroll w-[80px] flex justify-center items-center flex-col">
+          {history.map((el, index) => (
+            <div key={index}>{el}</div>
+          ))}
+        </div>
+        <div className="text-5xl mt-20">{total}</div>
+        <div className="flex flex-wrap gap-2 items-center justify-center">
+          {values.map((value, index) => (
+            <button
+              key={index}
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded active:bg-blue-400 transition-colors w-20 h-20 flex items-center justify-center"
+              onClick={() => {
+                setHistory([...history, value])
+                setTotal(total + value)
+              }}
+            >
+              {valueFilter(value)}
+            </button>
+          ))}
+          <button
+            className="bg-red-500 text-white font-medium py-2 px-4 rounded active:bg-red-400 transition-colors w-20 h-20 flex items-center justify-center text-xl"
+            onClick={() => {
+              setTotal(0)
+              setHistory([])
+            }}
+          >
+            Clear
+          </button>
+          <button
+            className="bg-emerald-500 text-white font-medium py-2 px-4 rounded active:bg-red-400 transition-colors w-20 h-20 flex items-center justify-center text-xl"
+            onClick={() => {
+              setTotal(history.length ? total - history[history.length - 1] : 0)
+              setHistory(history.slice(0, history.length - 1))
+            }}
+          >
+            Undo
+          </button>
+        </div>
+      </div>
+    </main>
+  )
+}
