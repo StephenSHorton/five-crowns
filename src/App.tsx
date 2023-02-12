700db9132451199bb31c39048ec4c3f5308c3448
StephenSHorton <4stephenhorton@gmail.com>
2023-02-12 14:12:54 -0700

    save state to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 //no aces or 2's
 //jokers are
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const values = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
@@ -22,6 +22,32 @@ function valueFilter(value: number) {
 export default function App() {
   const [history, setHistory] = useState<number[]>([])
   const [total, setTotal] = useState(0)
+  const [initialLoad, setInitialLoad] = useState(true)
+
+  useEffect(() => {
+    console.log(initialLoad)
+    if (initialLoad) {
+      const unmarshalledState = localStorage.getItem('five_crowns_state')
+      if (!unmarshalledState) return
+      const fiveCrownsState = JSON.parse(unmarshalledState)
+      if (!fiveCrownsState) return
+      if (fiveCrownsState.history && fiveCrownsState.total) {
+        console.log(fiveCrownsState)
+        setHistory(fiveCrownsState.history)
+        setTotal(fiveCrownsState.total)
+      }
+      setInitialLoad(false)
+    } else {
+      localStorage.setItem(
+        'five_crowns_state',
+        JSON.stringify({
+          history,
+          total,
+        }),
+      )
+    }
+  }, [history, total, initialLoad])
+
   return (
     <main className="w-screen h-screen">
       <div className="flex flex-col justify-around h-full items-center relative">
